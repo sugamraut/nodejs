a0eab6e779b6d0857392a27fdf88c7a1e15fb392
Sugam Raut <sugamrautbim@gmail.com>
2024-08-06 22:45:09 +0545

    understand Validation, Login Implementation and more

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { users } = require('./model/index')
 const app = express()
 const bcrypt= require('bcrypt')
+const { where } = require('sequelize')
 
 require("./model/index")
 // const app = require("express")()
@@ -27,6 +28,19 @@ app.get("/register",(req,res)=>{
 
 app.post("/register",async(req,res)=>{
     const {username,password, email}=req.body
+    if(!username||!password||!email){
+        return res.send("please provide userna,email,password")
+    }
+    //client side validation
+    /** const data=await users.findAll({
+        where:{
+            email:email
+        }
+    })
+    if (data.length>0){
+        return res.send("Already registered email")
+    }*/
+    
     await users.create({
         email,
         password :bcrypt.hashSync(password,10),
@@ -37,6 +51,31 @@ app.post("/register",async(req,res)=>{
 
 app.get("/login",(req,res)=>{
     res.render('auth/login')
+})
+app.post("/login", async (req,res)=>{
+const {email,password}=req.body
+if(!email||!password){
+    return res.send("please provide eamol,password")
+}
+//email check
+const [data]= await users.findAll({
+    where:{
+        email:email
+    }
+})
+if(data){
+    //next password check password
+    const isMAtched=bcrypt.compareSync(password,data.password)
+    if(isMAtched){
+        res.send("Logged in success")
+    }else{
+        res.send("Invalide password")
+    }
+}
+else{
+    res.send("no user with that email")
+}
+
 })
 
 
